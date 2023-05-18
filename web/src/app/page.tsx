d94a15e2b30f933157cdd064738c4efaed545ad4
Filippe Braga <filippeffx@hotmail.com>
2023-05-18 17:27:02 -0300

    tailwind colors and icon

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -2,7 +2,9 @@ export default function Home() {
   return (
     <div className="grid h-screen items-center justify-center bg-zinc-950 p-6 text-zinc-50">
       <h1 className="text-4xl font-bold">Sua cápsula do tempo</h1>
-      <h1 className="font-alt text-4xl font-bold">Sua cápsula do tempo</h1>
+      <h1 className="font-alt text-4xl font-bold text-green-400">
+        Sua cápsula do tempo
+      </h1>
     </div>
   )
 }
