638d28cc0d147585bd17dcfcf0c6bf11aea9fd11
Filippe Braga <filippeffx@hotmail.com>
2023-05-19 12:49:49 -0300

    cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,12 @@
+import cors from '@fastify/cors'
 import fastify from 'fastify'
 import { memoriesRoutes } from './routes/memories'
 
 const app = fastify()
+
+app.register(cors, {
+  origin: true, // with this option as true all URLs of frontend can access our backend, otherwise use a array to allow only the necessary
+})
 
 app.register(memoriesRoutes)
 
