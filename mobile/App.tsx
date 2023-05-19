7f46cc0d1dac988eb860f7854494a66595b05f34
Filippe Braga <filippeffx@hotmail.com>
2023-05-18 22:30:09 -0300

    background blur

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar'
-import { Text, View } from 'react-native'
+import { ImageBackground, Text } from 'react-native'
 
 import {
   Roboto_400Regular,
@@ -8,6 +8,8 @@ import {
 } from '@expo-google-fonts/roboto'
 
 import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
+
+import blurBg from './src/assets/bg-blur.png'
 
 export default function App() {
   const [hasLoadedFonts] = useFonts({
@@ -21,9 +23,13 @@ export default function App() {
   }
 
   return (
-    <View className="flex-1 items-center justify-center bg-gray-900">
-      <Text className="font-title text-5xl text-gray-50">Rocketseat</Text>
+    <ImageBackground
+      source={blurBg}
+      className="relative flex-1 items-center bg-gray-900"
+      imageStyle={{ position: 'absolute', left: '-100%' }}
+    >
+      <Text className="font-title text-5xl text-gray-50"></Text>
       <StatusBar style="light" translucent />
-    </View>
+    </ImageBackground>
   )
 }
