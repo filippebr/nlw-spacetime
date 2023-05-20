de726984948cb913a81b8b243b957116f7741b47
Filippe Braga <filippeffx@hotmail.com>
2023-05-20 16:02:24 -0300

    upload routes initial

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,12 +2,16 @@ import 'dotenv/config'
 
 import cors from '@fastify/cors'
 import jwt from '@fastify/jwt'
+import multipart from '@fastify/multipart'
 import fastify from 'fastify'
 
 import { authRoutes } from './routes/auth'
 import { memoriesRoutes } from './routes/memories'
+import { uploadRoutes } from './routes/upload'
 
 const app = fastify()
+
+app.register(multipart)
 
 app.register(cors, {
   origin: true, // with this option as true all URLs of frontend can access our backend, otherwise use a array to allow only the necessary
@@ -18,6 +22,7 @@ app.register(jwt, {
 })
 
 app.register(authRoutes)
+app.register(uploadRoutes)
 app.register(memoriesRoutes)
 
 app
