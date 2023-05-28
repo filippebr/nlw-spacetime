2c5a1bb52bb2764a17af37c4596fa2eb66d45ba9
Filippe Braga <filippeffx@hotmail.com>
2023-05-27 23:49:47 -0300

    memory edit web structure

--- web/src/app/memories/[id]/page.tsx
+++ web/src/app/memories/[id]/page.tsx
@@ -2,6 +2,7 @@ import { EmptyMemories } from '@/components/EmptyMemories'
 import dayjs from 'dayjs'
 import ptBr from 'dayjs/locale/pt-br'
 import { cookies } from 'next/headers'
+import Image from 'next/image'
 
 dayjs.locale(ptBr)
 
@@ -14,6 +15,9 @@ interface MemoryProps {
 export default async function Memory({ params }: MemoryProps) {
   const isAuthenticated = cookies().has('token')
 
+  const imageUrl =
+    'http://res.cloudinary.com/dcq0bdo1i/image/upload/v1685153853/nlw-spacetime/muem4pv6esydngora0ad.png'
+
   if (!isAuthenticated) {
     return <EmptyMemories />
   }
@@ -24,7 +28,21 @@ export default async function Memory({ params }: MemoryProps) {
 
   return (
     <div className="flex flex-col gap-10 p-8">
-      <h1>Memory: {params.id}</h1>
+      <div className="space-y-4">
+        <time className="-ml-8 flex items-center gap-2 text-sm text-gray-100 before:h-px before:w-5 before:bg-gray-50">
+          {dayjs(Date.now()).format('D[ de ]MMMM[, ]YYYY')}
+        </time>
+
+        <Image
+          src={imageUrl}
+          width={592}
+          height={280}
+          alt=""
+          className="w-64 rounded-lg object-cover"
+        />
+
+        <p className="text-lg leading-relaxed text-gray-100">{params.id}</p>
+      </div>
     </div>
   )
 }
