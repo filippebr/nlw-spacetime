a0171571a2afd6e2c3f48b4f4fd76590465d2ede
Filippe Braga <filippeffx@hotmail.com>
2023-05-29 15:29:54 -0300

    unique memory page destructured var, token

--- web/src/app/memories/[id]/page.tsx
+++ web/src/app/memories/[id]/page.tsx
@@ -21,42 +21,30 @@ export default async function PageMemory({ params }: MemoryProps) {
   }
   const token = cookies().get('token')?.value
 
-  console.log(token)
+  const memory = await api.get(`/memories/${params.id}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  })
 
-  let memory
-
-  if (token) {
-    try {
-      memory = await api.get(`/memories/${params.id}`, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      })
-      console.log(memory.data)
-    } catch (error) {
-      // Handle the error
-      console.error(error)
-    }
-  }
+  const { id, coverUrl, content, createdAt } = memory?.data
 
   return (
     <div className="flex flex-col gap-10 p-8">
-      <div key={memory?.data.id} className="space-y-4">
+      <div key={id} className="space-y-4">
         <time className="-ml-8 flex items-center gap-2 text-sm text-gray-100 before:h-px before:w-5 before:bg-gray-50">
-          {dayjs(memory?.data.createdAt).format('D[ de ]MMMM[, ]YYYY')}
+          {dayjs(createdAt).format('D[ de ]MMMM[, ]YYYY')}
         </time>
 
         <Image
-          src={memory?.data.coverUrl}
+          src={coverUrl}
           width={592}
           height={280}
           alt=""
           className="w-64 rounded-lg object-cover"
         />
 
-        <p className="text-lg leading-relaxed text-gray-100">
-          {memory?.data.content}
-        </p>
+        <p className="text-lg leading-relaxed text-gray-100">{content}</p>
       </div>
     </div>
   )
