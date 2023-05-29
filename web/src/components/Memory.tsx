6d19270e3d13672eb6c2b727593435811391397e
Filippe Braga <filippeffx@hotmail.com>
2023-05-29 20:43:59 -0300

    install react-datepicker  web

--- web/src/components/Memory.tsx
+++ web/src/components/Memory.tsx
@@ -26,7 +26,7 @@ export default function Memory({
         className="flex items-center gap-2  text-sm text-gray-200 hover:text-gray-100"
       >
         Voltar
-        <ArrowLeft className="h-4 w-4 " />
+        <ArrowLeft className="h-4 w-4" />
       </Link>
       <div key={id} className="space-y-4">
         <time className="-ml-8 flex items-center gap-2 text-sm text-gray-100 before:h-px before:w-5 before:bg-gray-50">
