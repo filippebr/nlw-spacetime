9b5573b67590b1b56b8dcac638188c7e8d6dca3c
Filippe Braga <filippeffx@hotmail.com>
2023-05-29 15:11:17 -0300

    unique memory web

--- web/src/app/memories/[id]/page.tsx
+++ web/src/app/memories/[id]/page.tsx
@@ -1,10 +1,11 @@
 import { EmptyMemories } from '@/components/EmptyMemories'
 import { api } from '@/lib/api'
-// import dayjs from 'dayjs'
-// import ptBr from 'dayjs/locale/pt-br'
+import dayjs from 'dayjs'
+import ptBr from 'dayjs/locale/pt-br'
 import { cookies } from 'next/headers'
+import Image from 'next/image'
 
-// dayjs.locale(ptBr)
+dayjs.locale(ptBr)
 
 interface MemoryProps {
   params: {
@@ -39,29 +40,24 @@ export default async function PageMemory({ params }: MemoryProps) {
   }
 
   return (
-    <h1>Memory ${params.id}</h1>
-    // <div key={params.id} className="space-y-4">
-    //   <time className="-ml-8 flex items-center gap-2 text-sm text-gray-100 before:h-px before:w-5 before:bg-gray-50">
-    //     {dayjs(createdAt).format('D[ de ]MMMM[, ]YYYY')}
-    //   </time>
+    <div className="flex flex-col gap-10 p-8">
+      <div key={memory?.data.id} className="space-y-4">
+        <time className="-ml-8 flex items-center gap-2 text-sm text-gray-100 before:h-px before:w-5 before:bg-gray-50">
+          {dayjs(memory?.data.createdAt).format('D[ de ]MMMM[, ]YYYY')}
+        </time>
 
-    //   <Image
-    //     src={coverUrl}
-    //     width={592}
-    //     height={280}
-    //     alt=""
-    //     className="w-64 rounded-lg object-cover"
-    //   />
+        <Image
+          src={memory?.data.coverUrl}
+          width={592}
+          height={280}
+          alt=""
+          className="w-64 rounded-lg object-cover"
+        />
 
-    //   <p className="text-lg leading-relaxed text-gray-100">{excerpt}</p>
-
-    //   <Link
-    //     href={`/memories/${id}`}
-    //     className="flex items-center gap-2  text-sm text-gray-200 hover:text-gray-100"
-    //   >
-    //     Ler mais
-    //     <ArrowRight className="h-4 w-4 " />
-    //   </Link>
-    // </div>
+        <p className="text-lg leading-relaxed text-gray-100">
+          {memory?.data.content}
+        </p>
+      </div>
+    </div>
   )
 }
