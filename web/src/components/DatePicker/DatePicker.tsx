d6dcc894109ade7ac53c62e0b6e37e5b220f0f1a
Filippe Braga <filippeffx@hotmail.com>
2023-05-31 21:08:11 -0300

    converted DatePicker function web

--- web/src/components/DatePicker/DatePicker.tsx
+++ web/src/components/DatePicker/DatePicker.tsx
@@ -1,16 +1,16 @@
-import { FC, useState } from 'react'
+import { useState } from 'react'
 import ReactCalendar from 'react-calendar'
 
 import './datePicker.css'
-
-interface dateProps {}
 
 interface DateType {
   justDate: Date | null
   dateTime: Date | null
 }
 
-export const DatePicker: FC<dateProps> = ({}) => {
+type DatePickerProps = {}
+
+export function DatePicker(props: DatePickerProps) {
   const [date, setDate] = useState<DateType>({
     justDate: null,
     dateTime: null,
